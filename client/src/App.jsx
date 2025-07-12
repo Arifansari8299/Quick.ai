70981b3299d8c150b8968b0374863915fdc073c7
Arif Ansari <arif8299309169@gmail.com>
2025-07-12 11:20:04 +0530

    Setup tailwind with vite and React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex h-screen items-center justify-center bg-gray-400">
+      <div className="p-6 bg-gray-400 rounded shadow">
+        <h1 className="text-8xl font-bold text-gray-800">🚀 Quick.ai-saas-app</h1>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
